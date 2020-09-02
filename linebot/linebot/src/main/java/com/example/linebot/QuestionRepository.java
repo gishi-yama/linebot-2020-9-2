24ca5454789630cf55b16733947130ffd4443015
Hiroto Yamakawa <1794095+gishi-yama@users.noreply.github.com>
2020-09-02 19:37:27 +0900

    省略してたのを修正

--- linebot/linebot/src/main/java/com/example/linebot/QuestionRepository.java
+++ linebot/linebot/src/main/java/com/example/linebot/QuestionRepository.java
@@ -1,5 +1,6 @@
 package com.example.linebot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -12,6 +13,7 @@ public class QuestionRepository {
 
   private final JdbcTemplate jdbcTemplate;
 
+  @Autowired
   public QuestionRepository(JdbcTemplate jdbcTemplate) {
     this.jdbcTemplate = jdbcTemplate;
   }
